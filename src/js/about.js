ab7d7e46540f9ae730b7be82066c57c38f4cf4da
akmalworkfront <akmal.work.front@gmail.com>
2026-08-06 16:11:22 +0600

    контрольная точка

--- src/js/about.js
+++ src/js/about.js
@@ -1,8 +1,14 @@
 import { initHeader } from "./header-fixed?aboutPage";
 import { initMenu } from "./menu?aboutPage";
+import { initSliders } from "./slider?aboutPage";
 import "/sass/about.sass";
 
 document.addEventListener("DOMContentLoaded", () => {
   initHeader();
   initMenu();
+  if ("requestIdleCallback" in window) {
+    requestIdleCallback(() => initSliders(), { timeout: 1000 });
+  } else {
+    setTimeout(initSliders, 200);
+  }
 });
